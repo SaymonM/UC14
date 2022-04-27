3315ddccd6030852e4d42a1f6f5069f07c331f24
SaymonM <saymonmeura@hotmail.com>
2022-04-27 16:46:38 -0400

    UC14 ER1

---
+++ Chapter/Chapter/Contexts/ChapterContext.cs
@@ -0,0 +1,30 @@
+﻿using Chapter.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Chapter.Contexts
+{
+    public class ChapterContext : DbContext
+    {
+        public ChapterContext()
+        {
+        }
+        public ChapterContext(DbContextOptions <ChapterContext> options) : base(options)
+        {
+        }
+        // vamos utilizar esse método para configurar o banco de dados
+        protected override void
+        OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                // cada provedor tem sua sintaxe paraespecificação
+
+                optionsBuilder.UseSqlServer("Data Source = DESKTOP-I2Q4UFF\\FILEDIRECTOR; initial catalog = Chapter; User Id = sa; Password = 1");
+            }
+        }
+        // dbset representa as entidades que serão utilizadas nas operações de leitura, criação, atualização e deleção
+
+        public DbSet<Livro> Livros { get; set; }
+    }
+}
+
